587dca51035ee484e5368a90fbdf83c511272b2f
Daniel-Kigozi <kdanalex69@gmail.com>
2023-01-04 17:17:18 +0300

    added api for post and get

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+import './modules/post.js';
+import getScores from './modules/get.js';
+
+
+const Resultscored = document.querySelector('.result-scored');
+const Refresh = document.querySelector('#btnrefresh');
+Refresh.addEventListener('click', async () => {
+  const scoredResults = await getScores();
+  Resultscored.innerHTML = '';
+  scoredResults.forEach((element) => {
+    Resultscored.innerHTML += `
+          <p> ${element.user}: ${element.score}</p>
+          `;
+  });
+});
+window.onload = async () => {
+  const scoredResults = await getScores();
+  Resultscored.innerHTML = '';
+  scoredResults.forEach((element) => {
+    Resultscored.innerHTML += `
+          <p> ${element.user}: ${element.score}</p>
+          `;
+  });
+};
